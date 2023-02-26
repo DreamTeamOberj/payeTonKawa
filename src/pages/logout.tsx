51cb1b299ed2dfb8211847c79fd492fe6f13a535
Gabriel <gabifiil68@gmail.com>
2023-02-26 20:15:26 +0100

    Add logout page

---
+++ src/pages/logout.tsx
@@ -0,0 +1,27 @@
+import {
+    IonButton,
+    IonContent,
+    IonGrid,
+    IonRow,
+    IonHeader,
+    IonInput,
+    IonItem,
+    IonLabel,
+    IonList,
+    IonPage,
+    IonTitle,
+    IonToolbar
+} from '@ionic/react';
+
+import './styles/login.css';
+import React from "react";
+
+const Logout: React.FC = () => {
+    return (
+        <IonPage>
+
+        </IonPage>
+    );
+};
+
+export default Logout;
