60b545f4fe65036ec9109c1c99a0285db8498115
RedDTG <a.deyris85@gmail.com>
2023-02-26 16:16:03 +0100

    adding redirect and product page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,37 +1,40 @@
 import {
-    IonButton,
     IonContent,
-    IonGrid,
-    IonRow,
-    IonHeader,
-    IonInput,
-    IonItem,
-    IonLabel,
-    IonList,
     IonPage,
-    IonTitle,
-    IonToolbar, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonSpinner
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent,
+    IonSpinner
 } from '@ionic/react';
 
+import { useHistory } from 'react-router-dom';
 import './styles/home.css';
 import FetchProducts from "../services/fetchProducts";
 import React from "react";
 
 const Home: React.FC = () => {
     const {data} = FetchProducts("https://615f5fb4f7254d0017068109.mockapi.io/api/v1/products");
+    const history = useHistory();
 
+    const goToProduct = (id: string) => {
+        history.push(`/product/${id}`);
+    }
+    
     if (!data) {
-        return <IonPage>
-            <IonContent class="spinner">
-                <IonSpinner  name="crescent"></IonSpinner>
-            </IonContent>
-        </IonPage>;
+        return (
+            <IonPage>
+                <IonContent class="spinner">
+                    <IonSpinner  name="crescent"></IonSpinner>
+                </IonContent>
+            </IonPage>
+        );
     } else {
         return (
             <IonPage>
                 <IonContent className="ion-padding">
                     {data.map((product: any) =>
-                        <IonCard key={product?.id}>
+                        <IonCard key={product?.id} onClick={() => goToProduct(product.id)}>
                             <IonCardHeader>
                                 <IonCardTitle>{product?.name}</IonCardTitle>
                             </IonCardHeader>
---
+++ src/pages/product.tsx
@@ -0,0 +1,47 @@
+import {
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent,
+    IonContent,
+    IonPage,
+    IonSpinner,
+} from '@ionic/react';
+
+import './styles/product.css';
+import FetchProducts from "../services/fetchProducts";
+import { useParams } from "react-router-dom";
+
+
+const Product: React.FC = () => {
+
+    const { id } : any = useParams();
+
+    const { data } : any = FetchProducts("https://615f5fb4f7254d0017068109.mockapi.io/api/v1/products/" + id);
+
+    
+    if (!data) {
+        return <IonPage>
+            <IonContent class="spinner">
+                <IonSpinner  name="crescent"></IonSpinner>
+            </IonContent>
+        </IonPage>;
+    } else {
+        const product = data
+        return (
+            <IonPage>
+                <IonContent className="ion-padding">
+                    <div>
+                        <h1>{product.name}</h1>
+                        <p>Price: {product.details.price}</p>
+                        <p>Description: {product.details.description}</p>
+                        <p>Color: {product.details.color}</p>
+                        <p>Stock: {product.stock}</p>
+                    </div>
+                </IonContent>
+            </IonPage>
+        );
+    }
+};
+
+export default Product;
