235675a5a6d726a002ef99968a7dac1f5ff75b73
M.Filiot <gabifiil68@gmail.com>
2023-02-28 15:53:10 +0100

    Change profile and login pages

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -10,15 +10,23 @@ import {
     IonList,
     IonPage,
     IonTitle,
-    IonToolbar
+    IonToolbar,
+    IonAvatar, IonCard
 } from '@ionic/react';
 
 import React from "react";
+import './styles/profile.css';
 
 const Profile: React.FC = () => {
     return (
         <IonPage>
-            <IonContent className="ion-padding">
+            <IonContent className="ion-padding ion-align-items-center">
+                <IonCard className="card-profile">
+                    <h2>Profile</h2>
+                    <IonAvatar>
+                    <img alt="Silhouette of a person's head" src="https://ionicframework.com/docs/img/demos/avatar.svg" />
+                    </IonAvatar>
+                </IonCard>
             </IonContent>
         </IonPage>
     );
